abbd51fdfb0b3910da39cea6b8b125c296559f3a
kalluraghuveerreddy <kalluraghureddy@gmail.com>
2024-10-19 10:26:14 +0530

    Removed the unnecessary imports

--- src/main/java/com/tech/userinfo/UserinfoApplication.java
+++ src/main/java/com/tech/userinfo/UserinfoApplication.java
@@ -2,10 +2,6 @@ package com.tech.userinfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import java.util.Arrays;
-import java.util.stream.Stream;
 
 @SpringBootApplication
 public class UserinfoApplication {
